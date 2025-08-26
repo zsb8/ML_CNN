18347d6617dc05a7f0dfe9f29e375bae2f0a17fb
zsb8 <songbin.zhang@gmail.com>
2025-08-26 18:19:01 -0400

    upload

---
+++ Image_Classification/digit_recognition_custom_net/net_class.py
@@ -0,0 +1,23 @@
+import torch.nn as nn
+import torch.nn.functional as F
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5)  # 增加通道数
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5)  # 增加通道数
+        self.fc1 = nn.Linear(in_features=64 * 5 * 5, out_features=512)  # 增加神经元
+        self.fc2 = nn.Linear(in_features=512, out_features=128)
+        self.fc3 = nn.Linear(in_features=128, out_features=10)
+        self.dropout = nn.Dropout(0.25)  # 添加dropout
+
+    def forward(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), kernel_size=2)
+        x = self.dropout(x)
+        x = F.max_pool2d(F.relu(self.conv2(x)), kernel_size=2)
+        x = self.dropout(x)
+        x = x.view(-1, 64 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
